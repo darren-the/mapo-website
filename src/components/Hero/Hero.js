bc1a347c40c3615c499fb49710c3a42c8e4ad362
darren <darren.the7@gmail.com>
2022-07-15 14:45:34 +1000

    finished hero mobile version without bg gradient

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,35 +3,35 @@ import logo from '../../assets/logo.svg'
 import heroRight from '../../assets/test.svg'
 import Button from '../Button/Button'
 
+{/* 
+        <div className="hero-right">
+          <img src={heroRight} alt="hero right" id="hero-right-images"></img>
+        </div>
+<div className="hero-button-wrapper">
+  <Button text="See Plans"/>
+</div> */}
+
 const Hero = () => {
   return (
     <div className="hero">
 
       <div className="hero-container">
 
-        <div className="hero-left-container">
-          
-          <div className="hero-logo-wrapper">
-            <img src={logo} width="96" alt="hero logo" id="hero-logo"/>
-          </div>
-
-          <div className="hero-header">Designing timeless digital experiences</div>
-
-          <div className="hero-sub-header">Subscription UI/UX that fulfils all your design needs, whenever you need it!</div>
-          
-          <div className="hero-button-wrapper">
-            <Button text="See Plans"/>
-          </div>
-
+        <div className="hero-logo-wrapper">
+          <img src={logo} width="96" alt="hero logo" id="hero-logo"/>
         </div>
-
         <div className="hero-right">
           <img src={heroRight} alt="hero right" id="hero-right-images"></img>
         </div>
-
+        <div className="hero-header">Designing timeless digital experiences</div>
+        <div className="hero-sub-header">Subscription UI/UX that fulfils all your design needs, whenever you need it!</div>
+        <div className="hero-button-wrapper">
+          <Button text="See Plans"/>
+        </div>
+        
       </div>
 
-      <div className="pm-gradient-wrapper">
+      <div className="hero-gradient-wrapper">
           <div id="hero-gradient"></div>
       </div>
 
